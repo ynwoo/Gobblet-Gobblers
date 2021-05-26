38229cc18ccf785a6e7ada978a50733ab9918016
yoonwoo_kim <ynwookim98@gmail.com>
2021-05-26 21:44:35 +0900

    add Q_learning_player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import pygame  # 파이게임 패키지 임포트
 import numpy as np
 from pygame.locals import QUIT  # 파이게임의 기능 중 종료를 임포트
+from tqdm import tqdm
 sys.setrecursionlimit(5000)
 # 글로벌 변수 선언
 
@@ -644,6 +645,15 @@ def Monte_Carlo_player():
     winner, draw = end_check(array)
 
 
+def Q_player(action):
+    global turn_end, array
+    global winner, draw
+
+    set_action(action, array)
+    turn_end = True
+    winner, draw = end_check(array)
+
+
 def playout(temp_array, action, c_player):
     # 보드에 플레이어의 선택을 표시(만약 새로운 말을 놓는 거라면)
     if isinstance(action, int):
@@ -873,23 +883,231 @@ def array_to_display():
     pygame.display.update()
 
 
+class Q_learning_player:
+    def __init__(self):
+        self.name = "Q_player"
+        # Q-table을 딕셔너리로 정의
+        self.qtable = {}
+        # e-greedy 계수 정의
+        self.epsilon = 1
+        # 학습률 정의
+        self.learning_rate = 0.1
+        self.gamma = 0.9
+        self.print = False
+
+    # policy 에 따라 상태에 맞는 행동을 선택
+    def select_action(self, arr, c_player):
+        #         print("Q_learning_player select action")
+        # policy 에 따라 행동을 선택
+        action = self.policy(arr, c_player)
+        return action
+
+    def policy(self, arr, c_player):
+        # 행동 가능한 상태를 저장
+        available_action = get_action(c_player, arr)
+        # 행동 가능한 상태의 Q-value를 저장
+        qvalues = np.zeros(len(available_action))
+        board_r = arr.reshape(27)
+        # 행동 가능한 상태의 Q-value를 조사
+        for i, act in enumerate(available_action):
+            key = (tuple(board_r), act)
+
+            # 현재 상태를 경험한 적이 없다면(딕셔너리에 없다면) 딕셔너리에 추가(Q-value = 0)
+            if self.qtable.get(key) == None:
+                self.qtable[key] = 0
+            # 행동 가능한 상태의 Q-value 저장
+            qvalues[i] = self.qtable.get(key)
+
+        # e-greedy
+        # 가능한 행동들 중에서 Q-value 가 가장 큰 행동을 저장
+        greedy_action = np.argmax(qvalues)
+
+        # max Q-value와 같은 값이 여러개 있는지 확인한 후 double_check에 상태를 저장
+        double_check = (np.where(qvalues == np.max(qvalues), 1, 0))
+
+        # 여러개 있다면 중복된 상태중에서 다시 무작위로 선택
+        if np.sum(double_check) > 1:
+            double_check = double_check / np.sum(double_check)
+            greedy_action = np.random.choice(range(0, len(double_check)), p=double_check)
+
+        # e-greedy 로 행동들의 선택 확률을 계산
+        pr = np.zeros(len(available_action))
+
+        for i in range(len(available_action)):
+            if i == greedy_action:
+                pr[i] = 1 - self.epsilon + self.epsilon / len(available_action)
+            else:
+                pr[i] = self.epsilon / len(available_action)
+
+        action = np.random.choice(range(0, len(available_action)), p=pr)
+        return available_action[action]
+
+    def learn_qtable(self, board_backup, action_backup, arr, c_reward, c_player):
+        # 현재 상태와 행동을 키로 저장
+        key = (board_backup, action_backup)
+
+        winner, draw = end_check(arr)
+        # Q-table 학습
+        if winner is not None or draw:
+            # 게임이 끝났을 경우 학습
+            self.qtable[key] += self.learning_rate * (c_reward - self.qtable[key])
+        else:
+            # 게임이 진행중일 경우 학습
+            # 다음 상태의 max Q 값 계산
+            available_action = get_action(c_player, arr)
+
+            qvalues = np.zeros(len(available_action))
+            board_r = arr.reshape(27)
+            for i, act in enumerate(available_action):
+                next_key = (tuple(board_r), act)
+
+                # 다음 상태를 경험한 적이 없다면(딕셔너리에 없다면) 딕셔너리에 추가(Q-value = 0)
+                if self.qtable.get(next_key) == None:
+                    self.qtable[next_key] = 0
+                qvalues[i] = self.qtable.get(next_key)
+
+            # maxQ 조사
+            maxQ = np.max(qvalues)
+
+            # 게임이 진행중일 때 학습
+            self.qtable[key] += self.learning_rate * (c_reward + self.gamma * maxQ - self.qtable[key])
+
+
 def main():  # 메인함수
     new_game_window()  # 화면 초기화
     global player, turn_end  # player 1P:1p, 2P:2p
+    # Q-learning 플레이어 훈련
+    p1_Qplayer = Q_learning_player()
+    p2_Qplayer = Q_learning_player()
+
+    # 입실론은 0.5로 설장
+    p1_Qplayer.epsilon = 0.5
+    p2_Qplayer.epsilon = 0.5
+
+    p1_score = 0
+    p2_score = 0
+    draw_score = 0
+    max_learn = 100
+
+    for j in tqdm(range(max_learn)):
+        np.random.seed(j)
+        temp_array = np.arange(27).reshape(3, 3, 3)
+        for i in range(0, 3):
+            for j in range(0, 3):
+                for k in range(0, 3):
+                    temp_array[i][j][k] = 0
+
+        for i in range(10000):
+            # p1 행동 선택
+            c_player = 1
+            pos = p1_Qplayer.policy(temp_array, c_player)
+
+            # 현재 상태 s, 행동 a를 저장
+            board_r = temp_array.reshape(27)
+            p1_board_backup = tuple(board_r)
+            p1_action_backup = pos
+
+            if isinstance(pos, int):  # 숫자라면
+                board_r[pos] = c_player
+            else:  # 문자라면 -> 움직이는 액션이다
+                pos = pos.split('to')  # pos[0]가 지울 장소, pos[1]이 생길 장소
+                board_r[int(pos[0])] = 0
+                board_r[int(pos[1])] = c_player
+            temp_array = board_r.reshape(3, 3, 3)
+            w, d = end_check(temp_array)
+
+            # 게임이 종료상태라면 각 플레이어의 Q-table을 학습
+            if w is not None or d:
+                # 비겼으면 보수 0
+                if d:
+                    p1_Qplayer.learn_qtable(p1_board_backup, p1_action_backup, temp_array, 0, player)
+                    p2_Qplayer.learn_qtable(p2_board_backup, p2_action_backup, temp_array, 0, player)
+                    draw_score += 1
+                    break
+                elif w == "P2":  # p1이 두었는데 p2가 이기는 경우
+                    p1_Qplayer.learn_qtable(p1_board_backup, p1_action_backup, temp_array, -1, player)
+                    p2_Qplayer.learn_qtable(p2_board_backup, p2_action_backup, temp_array, 1, player)
+                    p2_score += 1
+                    break
+                # p1이 이겼으므로 보상 +1
+                # p2이 졌으므로 보상 -1
+                else:
+                    p1_Qplayer.learn_qtable(p1_board_backup, p1_action_backup, temp_array, 1, player)
+                    p2_Qplayer.learn_qtable(p2_board_backup, p2_action_backup, temp_array, -1, player)
+                    p1_score += 1
+                    break
+
+            # 게임이 끝나지 않았다면 p2의 Q-table을 학습 (게임 시작직후에는 p2는 학습할 수 없음)
+            if i != 0:
+                p2_Qplayer.learn_qtable(p2_board_backup, p2_action_backup, temp_array, -0.01, player)
+
+            # p2 행동 선택
+            c_player = -1
+            pos = p2_Qplayer.policy(temp_array, c_player)
+
+            board_r = temp_array.reshape(27)
+            p2_board_backup = tuple(board_r)
+            p2_action_backup = pos
+
+            if isinstance(pos, int):  # 숫자라면
+                board_r[pos] = c_player
+            else:  # 문자라면 -> 움직이는 액션이다
+                pos = pos.split('to')  # pos[0]가 지울 장소, pos[1]이 생길 장소
+                board_r[int(pos[0])] = 0
+                board_r[int(pos[1])] = c_player
+
+            temp_array = board_r.reshape(3, 3, 3)
+            w, d = end_check(temp_array)
+
+            if w is not None or d:
+                # 비겼으면 보수 0
+                if d:
+                    p1_Qplayer.learn_qtable(p1_board_backup, p1_action_backup, temp_array, 0, player)
+                    p2_Qplayer.learn_qtable(p2_board_backup, p2_action_backup, temp_array, 0, player)
+                    draw_score += 1
+                    break
+                elif w == "P1":  # p2가 두었는데 p1이 이기는 경우
+                    p1_Qplayer.learn_qtable(p1_board_backup, p1_action_backup, temp_array, 1, player)
+                    p2_Qplayer.learn_qtable(p2_board_backup, p2_action_backup, temp_array, -1, player)
+                    p1_score += 1
+                    break
+                # p2이 이겼으므로 보상 +1
+                # p1이 졌으므로 보상 -1
+                else:
+                    p1_Qplayer.learn_qtable(p1_board_backup, p1_action_backup, temp_array, -1, player)
+                    p2_Qplayer.learn_qtable(p2_board_backup, p2_action_backup, temp_array, 1, player)
+                    p2_score += 1
+                    break
+
+            # 게임이 끝나지 않았다면 p1의 Q-table 학습
+            p1_Qplayer.learn_qtable(p1_board_backup, p1_action_backup, temp_array, -0.01, player)
+            # # 1000 게임마다 게임 결과 표시
+    #         if j % 1000 == 0:
+    #             print("j = {} p1 = {} p2 = {} draw = {}".format(j, p1_score, p2_score, draw_score))
+    #             print("p1 len : {}".format(len(p1_Qplayer.qtable)))
+    #             print("p2 len : {}".format(len(p2_Qplayer.qtable)))
+
+    print("p1 = {} p2 = {} draw = {}".format(p1_score, p2_score, draw_score))
+    print("end train")
 
     while True:  # 화면을 계속 띄우기 위해
         if player == "P1":
             # 1플레이어 두는 곳.
-            # time.sleep(0.5)
+            time.sleep(0.5)
             # Random_player()
             # Human_player()
-            Monte_Carlo_player()
+            # Monte_Carlo_player()
+
+            act = p1_Qplayer.select_action(array, 1)
+            Q_player(act)
             turn_end = False
         else:
             # 2플레이어 두는 곳.
-            # time.sleep(0.5)
+            time.sleep(0.5)
             # Random_player()
-            Monte_Carlo_player()
+            # Monte_Carlo_player()
+            act = p2_Qplayer.select_action(array, -1)
+            Q_player(act)
             # Human_player()
             turn_end = False
 
@@ -898,7 +1116,7 @@ def main():  # 메인함수
         else:
             player = "P1"
         draw_status()
-
+        
         if winner or draw:
             reset_game()
 
